8e516c59644d372a435852cec8c5d32b44f14873
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2023-12-05 10:57:14 -0300

    Create Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+import "./header.css"
+
+const Header = () =>{
+    /*=============== Toggle Menu ===============*/
+    const[Toggle, showMenu] = useState(false);
+    
+  return (
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">
+                    Alves
+                </a>
+                
+                <div className={Toggle ? "nav__menu show-menu" : 
+                "nav__menu"}>
+                  <ul className="nav__list grid">
+                    <li className="nav__item">
+                        <a href="#home" className="nav__link active-link">
+                          <i className="uil uil-estate nav__icon"></i> Home
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#sobre" className="nav__link">
+                          <i className="uil uil-user nav__icon"></i> Sobre
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#skills" className="nav__link">
+                          <i className="uil uil-file-alt nav__icon"></i> Skills
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#serviços" className="nav__link">
+                          <i className="uil uil-briefcase-alt nav__icon"></i> Serviços
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#potfolio" className="nav__link">
+                          <i className="uil uil-scenery nav__icon"></i> Portfolio
+                        </a>
+                    </li>
+
+                    <li className="nav__item">
+                        <a href="#contact" className="nav__link">
+                          <i className="uil uil-message nav__icon"></i> Contato
+                        </a>
+                    </li>
+                  </ul>
+
+                  {/*=============== Fechar o menu ===============*/}
+                  <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+                </div> 
+                
+                {/*=============== Vai abrir menu ===============*/}
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                  <i className="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
+  );
+}
+
+export default Header;
