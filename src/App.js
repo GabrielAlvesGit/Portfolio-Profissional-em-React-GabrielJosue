ce0940b1e13aca9b3263acb559704ce73fc64911
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2023-12-08 16:56:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
-import Home from "./components/home/Home";
 import Sobre from "./components/about/Sobre";
+import Home from "./components/home/Home";
 
 const App = () => {
     return (
