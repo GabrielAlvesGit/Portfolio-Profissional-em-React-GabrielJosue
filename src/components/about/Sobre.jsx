e2e4d3ce12d833d37215a236439933ab2e880ed8
Gabriel Alves <102634725+GabrielAlvesGit@users.noreply.github.com>
2023-12-10 23:49:09 -0300

    Update Sobre.jsx
    Up texto  ilustrativo

--- src/components/about/Sobre.jsx
+++ src/components/about/Sobre.jsx
@@ -15,7 +15,7 @@ const Sobre = () => {
 
               <div className="sobre__data">
 
-                  <p className="sobre__descricao">Frontend developer, I create web pages with UI / UX user interface, I have years of experience and many clients are happy with the projects carried out.
+                  <p className="sobre__descricao">Frontend developer, I create web pages with UI / UX user interface.
                   </p>
 
                   <div className="sobre__skillSetBox">
